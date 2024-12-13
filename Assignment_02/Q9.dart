e9160ab9b7ab8f14586a4f15c38bf0a6e14cc746
MURTAZA NAEEM <murtaza.naeem23.15@gmail.com>
2024-12-13 11:26:05 +0500

    Message

--- Assignment_02/Q9.dart
+++ Assignment_02/Q9.dart
@@ -1,2 +1,7 @@
 /*Q.9: Given a list of integers, write a dart code that returns the maximum value 
 from the list. */
+
+void main() {
+  List numbers = [11, 37, 22, 43, 56, 98, 33];
+  print("Maximum value is: ${numbers.reduce((a, b) => a > b ? a : b)}");
+}
