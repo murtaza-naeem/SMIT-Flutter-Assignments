c383abcd414b9c7c7cd74f178b4f6610f2359774
MURTAZA NAEEM <murtaza.naeem23.15@gmail.com>
2024-12-13 10:28:03 +0500

    massage

--- Assignment_02/Q7.dart
+++ Assignment_02/Q7.dart
@@ -17,3 +17,4 @@ void main() {
   print("Added value: $val");
   print("After: $expenses");
 }
+ 
--- Assignment_02/Q8.dart
+++ Assignment_02/Q8.dart
@@ -7,3 +7,16 @@ List<Map<String, bool>> usersEligibility = [
 {'name': 'Sarah', 'eligible': true},
 {'name': 'Tom', 'eligible': false},
 ]; */
+
+void main(){
+  List<Map> usersEligibility = [
+    {'name': 'John', 'eligible': true},
+    {'name': 'Alice', 'eligible': false},
+    {'name': 'Mike', 'eligible': true},
+    {'name': 'Sarah', 'eligible': true},
+    {'name': 'Tom', 'eligible': false},
+  ];
+  usersEligibility.removeWhere((e) => e['eligible'] == "false");
+  print(usersEligibility);
+  
+}
